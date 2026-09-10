2bb0824d13c458ca1668655eedac274627f5c359
George Kerr <82538979+GermanKerrMan@users.noreply.github.com>
2026-09-10 11:36:19 +0100

    Reset population value to 0 (was higher for tests)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const game_data = {
     // POPULATION AND MONEY
-    population: 10000000000000000000000000000000000000000000000000,
+    population: 0,
     money: 0,
     money_per_second: 0, 
 
